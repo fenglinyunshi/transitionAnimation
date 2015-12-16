b1a4bbe853dff307d9eb46efdd86b85069b53997
fenglinyunshi <fenglinyunshi@126.com>
2015-12-16 17:02:56 +0800

    Initial Commit

---
+++ 转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by Dustin on 15/12/16.
+//  Copyright © 2015年 Dustin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转场动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  转场动画
+//
+//  Created by Dustin on 15/12/16.
+//  Copyright © 2015年 Dustin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
